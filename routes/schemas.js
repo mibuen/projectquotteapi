493909c473340dbc6e25bc1ead141b280ca6e522
Manuel Segovia <msegovia@mac.com>
2022-05-03 22:53:24 -0500

    changed schema

--- routes/schemas.js
+++ routes/schemas.js
@@ -1,20 +1,20 @@
 const { required } = require('joi')
 const Joi = require('joi')
 
-module.exports.baseSchema = Joi.object({
-  cotizacionId: Joi.alternatives(
-    Joi.number(),
-    Joi.string()
-      .required()
-      .pattern(/^[0-9]+$/),
-  ),
-  proyectoId: Joi.alternatives(
-    Joi.number(),
-    Joi.string()
-      .required()
-      .pattern(/^[0-9]+$/),
-  ),
-})
+// module.exports.baseSchema = Joi.object({
+//   cotizacionId: Joi.alternatives(
+//     Joi.number(),
+//     Joi.string()
+//       .required()
+//       .pattern(/^[0-9]+$/),
+//   ),
+//   proyectoId: Joi.alternatives(
+//     Joi.number(),
+//     Joi.string()
+//       .required()
+//       .pattern(/^[0-9]+$/),
+//   ),
+// })
 module.exports.fotoSchema = Joi.object({
   cotizacionId: Joi.alternatives(
     Joi.number(),
@@ -70,9 +70,3 @@ module.exports.verReporte = Joi.object({
       .pattern(/^[0-9]+$/),
   ),
 })
-
-// reporte:[{
-//   header:{rnum:1, fecha:'xxxx'},
-//   grupo1:{inicio:'wwww.png',final:'zzz.png'},
-//   grupo2:{inicio:"qqq.webp",final:'rrrr.png'}
-// }]
